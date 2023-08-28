b56d0f7ef3ca77504e67e6133b8ca05ecf40c051
guefra <123297340+guefra@users.noreply.github.com>
2023-08-28 10:45:10 +0200

    Make system tray (icon not yet provided)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
-from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication, QSystemTrayIcon
+
+# Create the icon
+icon = QIcon("icon.ico")
+
+# Create the tray
+tray = QSystemTrayIcon()
+tray.setIcon(icon)
+tray.setVisible(True)
 
 app = QApplication([])
 app.setQuitOnLastWindowClosed(False)
